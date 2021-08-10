7b3ed56bfc70b2d2aa22d62feb541c438b83f871
David Ávila Fuentes <david.avila.fuentes@gmail.com>
2021-08-09 18:25:17 -0600

    Update Semana #3

--- src/main/java/TestAccount.java
+++ src/main/java/TestAccount.java
@@ -1,17 +1,43 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
+import io.qameta.allure.Description;
+import org.openqa.selenium.*;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
-public class TestAccount {
+public class TestAccount extends BaseClass{
 
-    @Test
+    @Description("Validate test login was successful")
+    @Test(description = "Test Login Success")
     public void Test_Login_Successful(){
-        String pathToDriver = Test.class.getResource("/chromedriver.exe").getPath();
-        System.setProperty("webdriver.chrome.driver", pathToDriver);
-        WebDriver driver = new ChromeDriver();
+        String username = "david.avila@ucreativa.com";
+        String password = "asdf";
 
-        driver.get("https://demo.opencart.com");
+        driver.findElement(By.xpath("//*[@id=\"top-links\"]/ul/li[2]/a/span[1]")).click();
+        driver.findElement(By.linkText("Login")).click();
 
+        driver.findElement(By.name("email")).sendKeys(username);
+        driver.findElement(By.name("password")).sendKeys(password);
+        driver.findElement(By.cssSelector("[value='Login']")).click();
+
+        WebElement logOutButton = driver.findElement(By.linkText("Logout"));
+        Assert.assertTrue(logOutButton.isDisplayed());
     }
 
+    @Description("Validate that the login is working with non valid credentials")
+    @Test(description = "Test Login Not Success")
+    public void Test_Login_Unsuccessful(){
+        String username = "david.avila@ucreativa.com";
+        String password = "asdfasdf";
+        String expectedMessage = "warning: no match for e-mail address and/or password.";
+
+        //Go To Login Page
+        driver.findElement(By.xpath("//*[@id=\"top-links\"]/ul/li[2]/a/span[1]")).click();
+        driver.findElement(By.linkText("Login")).click();
+
+        driver.findElement(By.name("email")).sendKeys(username);
+        driver.findElement(By.name("password")).sendKeys(password);
+        driver.findElement(By.cssSelector("[value='Login']")).click();
+
+        WebElement alertMessage = driver.findElement(By.xpath("//div[contains(@class, 'alert-danger')]"));
+        Assert.assertEquals(expectedMessage.toLowerCase(), alertMessage.getText().toLowerCase().trim());
+    }
 }
